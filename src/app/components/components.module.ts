c8a746ed469cca3eb4ff5b597a31553ad5e9c5ba
Santiago Salazar Castaño <kalaxdj@gmail.com>
2021-04-21 13:47:54 -0500

    initial commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    HomeComponent 
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    LoginComponent,
+    HomeComponent 
+  ]
+})
+export class ComponentsModule { }
